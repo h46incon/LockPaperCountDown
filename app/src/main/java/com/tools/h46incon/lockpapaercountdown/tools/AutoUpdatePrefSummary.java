628642801b623329e393677c588d093bf5cf4627
h46incon <h46incon@gmail.com>
2014-08-26 14:45:44 +0800

    Add: some util

---
+++ app/src/main/java/com/tools/h46incon/lockpapaercountdown/tools/AutoUpdatePrefSummary.java
@@ -0,0 +1,98 @@
+package com.tools.h46incon.lockpapaercountdown.tools;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.preference.ListPreference;
+import android.preference.Preference;
+
+/**
+ * Created by Administrator on 2014/4/24.
+ */
+public class AutoUpdatePrefSummary {
+	public static interface GetPrefMsgCB{
+		public String onGetMeg(Preference pref);
+	}
+
+	public static boolean addAutoUpdate(Preference pref, final String template, final GetPrefMsgCB cb)
+	{
+		if (pref != null) {
+			setPrefSummary(pref, template, cb);
+
+			pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				@Override
+				public boolean onPreferenceChange(final Preference preference, Object newVal)
+				{
+					// The lPref's entry will not change before this callback return true.
+					// So need to change the summary in next message loop to get the correct entry.
+					UIHandler.post(new Runnable() {
+						@Override
+						public void run()
+						{
+							setPrefSummary(preference, template, cb);
+						}
+					});
+					return true;
+				}
+			});
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean addAutoUpdate(Preference pref, final int templateID, final GetPrefMsgCB cb)
+	{
+		String template;
+		if (templateID == 0) {
+			template = null;
+		} else {
+			template = mApplication.getContext().getString(templateID);
+		}
+
+		return addAutoUpdate(pref, template, cb);
+	}
+
+	public static String getPrefMsgToShown(Preference pref)
+	{
+		String msg = "";
+
+		if (pref instanceof ListPreference) {
+			ListPreference lpref = (ListPreference) pref;
+
+			try {
+				msg = lpref.getEntry().toString();
+			} catch (NullPointerException e) {
+				msg = "";
+			}
+		} else {
+			String key = pref.getKey();
+			msg = mApplication.getSharedPreferences().getString(key, "");
+		}
+
+		return msg;
+	}
+
+
+	//========== private
+	private static void setPrefSummary(Preference pref, String template, GetPrefMsgCB getPrefMsgCB)
+	{
+		String msgToShow;
+		if (getPrefMsgCB == null) {
+			msgToShow = getPrefMsgToShown(pref);
+		} else {
+			msgToShow = getPrefMsgCB.onGetMeg(pref);
+		}
+
+		String summary;
+		if (template == null) {
+			summary = msgToShow;
+		} else {
+			summary = String.format(template, msgToShow);
+		}
+
+		pref.setSummary(summary);
+	}
+
+
+	private final static Handler UIHandler = new Handler(Looper.getMainLooper());
+}
---
+++ app/src/main/java/com/tools/h46incon/lockpapaercountdown/tools/mApplication.java
@@ -0,0 +1,76 @@
+package com.tools.h46incon.lockpapaercountdown.tools;
+
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2014/4/11.
+ */
+public class mApplication extends Application {
+
+	public static void addOnAppInitCallBack(Runnable runnable)
+	{
+		onAppInitCB.add(runnable);
+		// if onCreate has been run, then run the callback here.
+		if (hasAppInited) {
+			runnable.run();
+		}
+	}
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		instance = this;
+		applicationContext = getApplicationContext();
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+		if (!hasAppInited) {
+			for (Runnable runnable : onAppInitCB) {
+				if (runnable != null) {
+					runnable.run();
+				}
+			}
+		}
+		hasAppInited = true;
+	}
+
+	public static boolean isServiceRunning(String serviceClassName)
+	{
+		ActivityManager activityManager =
+				(ActivityManager)instance.getSystemService(ACTIVITY_SERVICE);
+		List<ActivityManager.RunningServiceInfo> infoList
+				= activityManager.getRunningServices(Integer.MAX_VALUE);
+
+		for (ActivityManager.RunningServiceInfo info : infoList) {
+			if (info.service.getClassName().equals(serviceClassName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Context getContext()
+	{
+		return applicationContext;
+	}
+
+	public static SharedPreferences getSharedPreferences()
+	{
+		return sharedPreferences;
+	}
+
+	private static mApplication instance;
+	private static Context applicationContext;
+	private static SharedPreferences sharedPreferences;
+
+	private static Collection<Runnable> onAppInitCB = new LinkedList<Runnable>();
+	private static boolean hasAppInited = false;
+}
