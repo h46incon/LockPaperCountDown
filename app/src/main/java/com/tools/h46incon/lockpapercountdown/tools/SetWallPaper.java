29f7f5f187b1dedba3116a65fbc039da5d56953f
h46incon <h46incon@gmail.com>
2014-08-28 20:43:06 +0800

    Add method to get wallpaper size in class SetWallPaper

--- app/src/main/java/com/tools/h46incon/lockpapercountdown/tools/SetWallPaper.java
+++ app/src/main/java/com/tools/h46incon/lockpapercountdown/tools/SetWallPaper.java
@@ -1,13 +1,17 @@
 package com.tools.h46incon.lockpapercountdown.tools;
 
 import android.app.WallpaperManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 import com.tools.h46incon.lockpapercountdown.R;
 import com.tools.h46incon.lockpapercountdown.util.GetSPByID;
@@ -21,10 +25,57 @@ import java.lang.reflect.Method;
  * Created by h46incon on 2014/8/26.
  */
 public class SetWallPaper{
+	public static class Size{
+		public int weight;
+		public int height;
+	}
+
 	public static boolean couldSetLockPaper()
 	{
 		Method setLockPaperMethod = tryGetSetLockPaperMethod();
 		return setLockPaperMethod != null;
+	}
+
+	public static Size getWallPaperSize()
+	{
+		Size size = new Size();
+		size.weight = mWallManager.getDesiredMinimumWidth();
+		size.height = mWallManager.getDesiredMinimumHeight();
+		return size;
+	}
+
+	/*
+		Try return Lock Screen Paper Size according to API in MeiZu FlyMe OS first
+		or return size of screen in other ROMs
+	 */
+	public static Size getLockPaperSize()
+	{
+		Size size = new Size();
+		boolean hasGetSize = false;
+		Class<?> wallPaperMangerClass = WallpaperManager.class;
+
+		// Try get size from API
+		try {
+			Method getLockWallpaperDesiredMinimumWidthMethod =
+					wallPaperMangerClass.getMethod("getLockWallpaperDesiredMinimumWidth");
+			// It's just getlock... Not getLock...
+			Method getLockWallpaperDesiredMinimumHeightMethod =
+					wallPaperMangerClass.getMethod("getlockWallpaperDesiredMinimumHeight");
+
+			size.weight = (Integer) getLockWallpaperDesiredMinimumWidthMethod.invoke(mWallManager);
+			size.height = (Integer) getLockWallpaperDesiredMinimumHeightMethod.invoke(mWallManager);
+			hasGetSize = true;
+		} catch (IllegalAccessException ignored) {
+		} catch (InvocationTargetException ignored) {
+		} catch (NoSuchMethodException ignored) {
+		}
+
+		// Not successful, return size of screen
+		if (!hasGetSize) {
+			size = getScreenPixels();
+		}
+
+		return size;
 	}
 
 	/*
@@ -54,7 +105,6 @@ public class SetWallPaper{
 	 */
 	public static boolean updateLockPaper()
 	{
-		WallpaperManager mWallManager = WallpaperManager.getInstance(MyApplication.getContext());
 		Method setLockPaperMethod = tryGetSetLockPaperMethod();
 
 		if (setLockPaperMethod == null) {
@@ -81,8 +131,6 @@ public class SetWallPaper{
 
 	public static boolean updateWallPaper()
 	{
-		WallpaperManager mWallManager = WallpaperManager.getInstance(MyApplication.getContext());
-
 		int countDownNumber = getCountDownNumber();
 		Bitmap newb = getWallPaper("" + countDownNumber, 500, 800);
 
@@ -106,6 +154,35 @@ public class SetWallPaper{
 		} catch (NoSuchMethodException e) {
 			return null;
 		}
+	}
+
+	private static Size getScreenPixels()
+	{
+		WindowManager windowManager =
+				(WindowManager) appCont.getSystemService(Context.WINDOW_SERVICE);
+		Display display = windowManager.getDefaultDisplay();
+
+		Size size = new Size();
+		// For JellyBeans and onward
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+			DisplayMetrics metrics = new DisplayMetrics();
+			display.getRealMetrics(metrics);
+			size.weight = metrics.widthPixels;
+			size.height = metrics.heightPixels;
+		} else {
+			try {
+				Method mGetRawH = Display.class.getMethod("getRawHeight");
+				Method mGetRawW = Display.class.getMethod("getRawWidth");
+				size.weight = (Integer) mGetRawW.invoke(display);
+				size.height = (Integer) mGetRawH.invoke(display);
+			} catch (IllegalArgumentException ignored) {
+			} catch (IllegalAccessException ignored) {
+			} catch (InvocationTargetException ignored) {
+			} catch (NoSuchMethodException ignored) {
+			}
+		}
+
+		return size;
 	}
 
 	private static int getCountDownNumber()
@@ -157,5 +234,8 @@ public class SetWallPaper{
 	}
 
 	private static final String TAG = "SetWallPaper";
+	private static WallpaperManager mWallManager =
+			WallpaperManager.getInstance(MyApplication.getContext());
+	private static Context appCont = MyApplication.getContext();
 
 }
