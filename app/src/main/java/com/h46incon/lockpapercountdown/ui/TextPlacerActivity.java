2534b29e7d11e157db745ce61f95f5d96b82435d
h46incon <h46incon@gmail.com>
2014-08-31 08:45:03 +0800

    Implement color picker's event

--- app/src/main/java/com/h46incon/lockpapercountdown/ui/TextPlacerActivity.java
+++ app/src/main/java/com/h46incon/lockpapercountdown/ui/TextPlacerActivity.java
@@ -2,11 +2,16 @@ package com.h46incon.lockpapercountdown.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.h46incon.lockpapercountdown.R;
 
+import yuku.ambilwarna.AmbilWarnaDialog;
+
 /**
- * Created by Administrator on 2014/8/29.
+ * Created by h46incon on 2014/8/29.
  */
 public class TextPlacerActivity extends Activity{
 	@Override
@@ -14,5 +19,49 @@ public class TextPlacerActivity extends Activity{
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acivity_textplacer);
+		initViewVar();
+		initListener();
+
 	}
+
+	private void initViewVar()
+	{
+		pickColorBtn = (Button) findViewById(R.id.pick_color_btn);
+		pickFontBtn = (Button) findViewById(R.id.pick_font_btn);
+	}
+
+	private void initListener()
+	{
+		pickColorBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				AmbilWarnaDialog ambilWarnaDialog =
+						new AmbilWarnaDialog(TextPlacerActivity.this, selectedColor, true, mOnColorSelected);
+				ambilWarnaDialog.show();
+			}
+		});
+	}
+
+
+	private AmbilWarnaDialog.OnAmbilWarnaListener mOnColorSelected = new AmbilWarnaDialog.OnAmbilWarnaListener() {
+		@Override
+		public void onCancel(AmbilWarnaDialog dialog)
+		{
+			Log.d(TAG, "color picker dialog is cancel");
+		}
+
+		@Override
+		public void onOk(AmbilWarnaDialog dialog, int color)
+		{
+			selectedColor = color;
+			Log.d(TAG, "Select color" + color);
+		}
+	};
+
+	Button pickColorBtn;
+	Button pickFontBtn;
+
+	private final String TAG = "TextPlacerActivity";
+	private int selectedColor = 0xFFFF0000;     // Init selected color as RED
 }
